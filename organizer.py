3b8f496b38a0c934fa0f9983bd52ca6118ef9e1b
Yachika Chaudhary <134478035+YachikaChaudhary@users.noreply.github.com>
2026-06-28 23:21:37 +0530

    Create organizer.py

---
+++ organizer.py
@@ -0,0 +1,60 @@
+import os
+import shutil
+
+
+topics = {
+    "array": "Array",
+    "two sum": "Array",
+    "hash table": "Hash Table",
+    "binary search": "Binary Search",
+    "linked list": "Linked List",
+    "stack": "Stack",
+    "queue": "Queue",
+    "tree": "Trees",
+    "graph": "Graphs",
+    "dynamic programming": "Dynamic Programming",
+    "backtracking": "Backtracking",
+    "greedy": "Greedy",
+    "sliding window": "Sliding Window"
+}
+
+
+def detect_topic(folder):
+
+    name = folder.lower()
+
+    for keyword, topic in topics.items():
+        if keyword in name:
+            return topic
+
+    return "Others"
+
+
+
+for item in os.listdir():
+
+    if os.path.isdir(item) and item[0].isdigit():
+
+        topic = detect_topic(item)
+
+        destination = os.path.join(
+            "Topics",
+            topic,
+            item
+        )
+
+        if not os.path.exists(destination):
+
+            os.makedirs(
+                os.path.dirname(destination),
+                exist_ok=True
+            )
+
+            shutil.copytree(
+                item,
+                destination
+            )
+
+            print(
+                f"Added {item} -> {topic}"
+            )
